6a53560e21f2782d6340d5e42145f018aff7da09
Chris Hartung <chartung@seas.upenn.edu>
2020-10-28 16:55:51 -0700

    Updates

---
+++ precache-manifest.dcec2eaa46e07483d6d738d167fc0016.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61110ec1f6595fa40fc1eef576a51210",
+    "url": "https://chartung17.github.io/election-app/index.html"
+  },
+  {
+    "revision": "75716bcaadf551053d1a",
+    "url": "https://chartung17.github.io/election-app/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "72025de237f8105708cc",
+    "url": "https://chartung17.github.io/election-app/static/css/main.14345fe9.chunk.css"
+  },
+  {
+    "revision": "75716bcaadf551053d1a",
+    "url": "https://chartung17.github.io/election-app/static/js/2.ad2f2fa5.chunk.js"
+  },
+  {
+    "revision": "abe4cd3f0ec916cce1b4146c262829c7",
+    "url": "https://chartung17.github.io/election-app/static/js/2.ad2f2fa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72025de237f8105708cc",
+    "url": "https://chartung17.github.io/election-app/static/js/main.5bcf6f9e.chunk.js"
+  },
+  {
+    "revision": "32e1c62b8a2a69f90371",
+    "url": "https://chartung17.github.io/election-app/static/js/runtime-main.2be59e3d.js"
+  },
+  {
+    "revision": "1d5d551052d6adc753621f2dc4c528d6",
+    "url": "https://chartung17.github.io/election-app/static/media/al_gore.1d5d5510.jpg"
+  },
+  {
+    "revision": "341e121230dfa6c53a8a948e4fe06ac6",
+    "url": "https://chartung17.github.io/election-app/static/media/barack_obama.341e1212.jpg"
+  },
+  {
+    "revision": "b07e12ff81258aae9c9450f43fd1e913",
+    "url": "https://chartung17.github.io/election-app/static/media/donald_trump.b07e12ff.jpg"
+  },
+  {
+    "revision": "433056d23a0564c4a344c81fe2a95422",
+    "url": "https://chartung17.github.io/election-app/static/media/george_bush.433056d2.jpg"
+  },
+  {
+    "revision": "3db6d42d34f61518b67061642594e13e",
+    "url": "https://chartung17.github.io/election-app/static/media/hillary_clinton.3db6d42d.jpg"
+  },
+  {
+    "revision": "f2b52f4d49b3c30007961fc1fb22eb15",
+    "url": "https://chartung17.github.io/election-app/static/media/john_kerry.f2b52f4d.jpg"
+  },
+  {
+    "revision": "37b176712acfc02ab4a56fd46dbb5d80",
+    "url": "https://chartung17.github.io/election-app/static/media/john_mccain.37b17671.jpg"
+  },
+  {
+    "revision": "c17a77bce5a25b62318418f324c5aa28",
+    "url": "https://chartung17.github.io/election-app/static/media/mitt_romney.c17a77bc.jpg"
+  }
+]);
