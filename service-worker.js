cf1505c3d56a85ee0f32aa32eababa77af3fee6f
Chris Hartung <chartung@seas.upenn.edu>
2020-10-28 16:58:44 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://chartung17.github.io/election-app/precache-manifest.dcec2eaa46e07483d6d738d167fc0016.js"
+  "/election-app/precache-manifest.91cfa08f8ba8fbefb2289cd9b8bc766d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://chartung17.github.io/election-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/election-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
