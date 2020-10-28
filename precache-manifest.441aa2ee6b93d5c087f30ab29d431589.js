20ff726fab6b38f2c62b1e687b607516a7e7a54f
Chris Hartung <chartung@seas.upenn.edu>
2020-10-28 14:26:52 -0700

    Updates

---
+++ precache-manifest.441aa2ee6b93d5c087f30ab29d431589.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5622672ddd6339a6e8915641148daf1a",
+    "url": "/election-app/index.html"
+  },
+  {
+    "revision": "ea4d43fa354b5129a415",
+    "url": "/election-app/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "667f2e41eda2caa09372",
+    "url": "/election-app/static/css/main.14345fe9.chunk.css"
+  },
+  {
+    "revision": "ea4d43fa354b5129a415",
+    "url": "/election-app/static/js/2.27a9111a.chunk.js"
+  },
+  {
+    "revision": "abe4cd3f0ec916cce1b4146c262829c7",
+    "url": "/election-app/static/js/2.27a9111a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "667f2e41eda2caa09372",
+    "url": "/election-app/static/js/main.7ee563e8.chunk.js"
+  },
+  {
+    "revision": "1aee47ee1cc1b068298f",
+    "url": "/election-app/static/js/runtime-main.a02a53fc.js"
+  },
+  {
+    "revision": "1d5d551052d6adc753621f2dc4c528d6",
+    "url": "/election-app/static/media/al_gore.1d5d5510.jpg"
+  },
+  {
+    "revision": "341e121230dfa6c53a8a948e4fe06ac6",
+    "url": "/election-app/static/media/barack_obama.341e1212.jpg"
+  },
+  {
+    "revision": "b07e12ff81258aae9c9450f43fd1e913",
+    "url": "/election-app/static/media/donald_trump.b07e12ff.jpg"
+  },
+  {
+    "revision": "433056d23a0564c4a344c81fe2a95422",
+    "url": "/election-app/static/media/george_bush.433056d2.jpg"
+  },
+  {
+    "revision": "3db6d42d34f61518b67061642594e13e",
+    "url": "/election-app/static/media/hillary_clinton.3db6d42d.jpg"
+  },
+  {
+    "revision": "f2b52f4d49b3c30007961fc1fb22eb15",
+    "url": "/election-app/static/media/john_kerry.f2b52f4d.jpg"
+  },
+  {
+    "revision": "37b176712acfc02ab4a56fd46dbb5d80",
+    "url": "/election-app/static/media/john_mccain.37b17671.jpg"
+  },
+  {
+    "revision": "c17a77bce5a25b62318418f324c5aa28",
+    "url": "/election-app/static/media/mitt_romney.c17a77bc.jpg"
+  }
+]);
