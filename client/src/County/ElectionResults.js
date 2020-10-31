ac77b34852e1540f41f0a488ed054e48618c83a7
islong <islong@seas.upenn.edu>
2020-10-31 11:59:44 -0400

    [County] refactor: election subpanels

---
+++ client/src/County/ElectionResults.js
@@ -0,0 +1,94 @@
+import React, {Fragment} from "react";
+import {CANDIDATE_IMAGE} from "./Constants";
+import {ASSETS_PATH} from "./County";
+
+export default class ElectionSummary extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let CANDIDATE_IMG_PATH = `${ASSETS_PATH}/images/candidate`;
+        let CHECKMARK_ICON = `${ASSETS_PATH}/icons/other/checkmark.svg`;
+
+        return (
+            <table className={"election-table"}>
+                <tbody>
+                {this.props.electionResult.map((value, index) => {
+                    let totalVote = parseInt(value["TotalVote"])
+                    let demPct = parseInt(value["DemVote"]) * 100 / totalVote;
+                    let repPct = parseInt(value["RepVote"]) * 100 / totalVote;
+                    let demBarWidth = isNaN(demPct) ? 0 : Math.round(demPct);
+                    let repBarWidth = isNaN(repPct) ? 0 : Math.round(repPct);
+
+                    return (
+                        <Fragment key={index}>
+                            <tr className={"election-table-year-container"}>
+                                <td/>
+                                <td className={"election-table-year"} colSpan={2}>
+                                    <h3>{value["Year"]}</h3></td>
+                                <td/>
+                            </tr>
+                            <tr className={"election-table-result-container"} key={index}>
+                                <td className={"headshot-container"}>
+                                    <img
+                                        src={`${CANDIDATE_IMG_PATH}/${CANDIDATE_IMAGE[value["DemCandidate"]]}`}
+                                        alt={value["DemCandidate"]}
+                                        className={"headshot"}
+                                    />
+                                    {value["DemCandidate"]}
+                                </td>
+                                <td className={"democrat-results-container"}>
+                                    {
+                                        (totalVote === 0 || demPct < repPct) ? null :
+                                            <img
+                                                src={CHECKMARK_ICON}
+                                                alt="" className={"democrat-vote-checkmark"}
+                                                style={{marginLeft: `${Math.min(77, 100 - demBarWidth + 5)}%`}}
+                                            />
+                                    }
+                                    <div className={"democrat-vote-bar"}
+                                         style={{marginLeft: `${100 - Math.round(demBarWidth)}%`}}>
+                                        &nbsp;
+                                    </div>
+                                    <div id={`elec-dem-pct-${index}`}>
+                                        {(totalVote === 0 ? 0 : demPct.toFixed(1)) + "%"}
+                                    </div>
+                                    <div id={`elec-dem-vote-${index}`}>
+                                        ({value["DemVote"]})
+                                    </div>
+                                </td>
+
+                                <td className={"republican-results-container"}>
+                                    {(totalVote === 0 || demPct > repPct) ? null :
+                                        <img src={CHECKMARK_ICON} alt=""
+                                             className={"republican-vote-checkmark"}
+                                             style={{marginRight: `${Math.min(77, 100 - repBarWidth)}%`}}
+                                        />
+                                    }
+                                    <div className={"republican-vote-bar"} style={{width: repBarWidth + '%'}}>
+                                        &nbsp;
+                                    </div>
+                                    <div id={`elec-rep-pct-${index}`}>
+                                        &nbsp; {(totalVote === 0 ? 0 : repPct.toFixed(1)) + "%"}
+                                    </div>
+                                    <div id={`elec-rep-vote-${index}`}>
+                                        &nbsp; ({value["RepVote"]})
+                                    </div>
+                                </td>
+                                <td className={"headshot-container"}>
+                                    <img
+                                        src={`${CANDIDATE_IMG_PATH}/${CANDIDATE_IMAGE[value["RepCandidate"]]}`}
+                                        alt={value["RepCandidate"]}
+                                        className={"headshot"}/>
+                                    {value["RepCandidate"]}
+                                </td>
+                            </tr>
+                        </Fragment>
+                    );
+                })}
+                </tbody>
+            </table>
+        );
+    }
+}
